279a2dc5e9c06c3103b9a426bb76a89cba8315c3
GET Academy <terje@kolderup.net>
2020-02-04 12:57:01 +0100

    x

---
+++ FootballGoals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace FootballGoals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var homeGoals = new int[12];
+            var awayGoals = new int[12];
+            Console.Write("Skriv først inn rekken du" +
+                          " har tippet, for eksempel HUBHUBHUBHUB: ");
+            var usersPlayedCombination = Console.ReadLine()?.ToUpper();
+            if (usersPlayedCombination == null
+                || usersPlayedCombination.Length != 12
+                || usersPlayedCombination.Any(c => !"HUB".Contains(c)))
+            {
+                Console.WriteLine("Ugyldig rekke.");
+                return;
+            }
+
+            while (true)
+            {
+                Console.Write("Skriv inn kampnr. og H eller B, for eksempel \"10H\":");
+                var input = Console.ReadLine()?.ToUpper();
+
+            }
+        }
+    }
+}
