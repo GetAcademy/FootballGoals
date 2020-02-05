680216b0ecac6b04eb11602d510566aa3c144795
GET Academy <terje@kolderup.net>
2020-02-05 14:20:37 +0100

    x

--- FootballGoals.UnitTest/MatchTest.cs
+++ FootballGoals.UnitTest/MatchTest.cs
@@ -28,5 +28,52 @@ namespace FootballGoals.UnitTest
             var textLine = match.GetTextLine();
             Assert.AreEqual("Kamp 11 0-0 U Riktig", textLine);
         }
+
+        [Test]
+        public void TestMatchWrongBet()
+        {
+            var match = new Match(11, 'H');
+            var textLine = match.GetTextLine();
+            Assert.AreEqual("Kamp 11 0-0 U Feil (Du tippet H)", textLine);
+            Assert.IsFalse(match.IsBetCorrect);
+        }
+
+        [Test]
+        public void TestMatchOneGoalHome()
+        {
+            var match = new Match(2, 'H');
+            var isSuccess = match.AddGoal('H');
+            var textLine = match.GetTextLine();
+            Assert.AreEqual("Kamp 02 1-0 H Riktig", textLine);
+            Assert.IsTrue(isSuccess);
+        }
+
+        [Test]
+        public void TestMatchOneGoalHome2()
+        {
+            var match = new Match(2, 'H');
+            var isSuccess = match.AddGoal('h');
+            var textLine = match.GetTextLine();
+            Assert.AreEqual("Kamp 02 1-0 H Riktig", textLine);
+            Assert.IsTrue(isSuccess);
+        }
+
+        [Test]
+        public void TestMatchOneGoalAway()
+        {
+            var match = new Match(2, 'B');
+            var isSuccess = match.AddGoal('B');
+            var textLine = match.GetTextLine();
+            Assert.AreEqual("Kamp 02 0-1 B Riktig", textLine);
+            Assert.IsTrue(isSuccess);
+        }
+
+        [Test]
+        public void TestMatchInvalidGoal()
+        {
+            var match = new Match(2, 'H');
+            var isSuccess = match.AddGoal('X');
+            Assert.IsFalse(isSuccess);
+        }
     }
 }
--- FootballGoals/Match.cs
+++ FootballGoals/Match.cs
@@ -6,18 +6,38 @@ namespace FootballGoals
 {
     public class Match
     {
-        private int _matchNo;
+        private readonly int _matchNo;
+        private readonly char _bet;
+        private int _homeGoals;
+        private int _awayGoals;
+
+        public char CurrentResult =>
+            _homeGoals == _awayGoals ? 'U' :
+            _homeGoals > _awayGoals ? 'H' :
+            'B';
+
+        public bool IsBetCorrect => _bet == CurrentResult;
 
         public Match(int matchNo, char bet)
         {
+            _bet = bet;
             _matchNo = matchNo;
         }
 
-        public bool IsBetCorrect = true;
-
         public string GetTextLine()
         {
-            return $"Kamp {_matchNo:D2} 0-0 U Riktig";
+            var isCorrectText = IsBetCorrect ? "Riktig" : $"Feil (Du tippet {_bet})";
+            return $"Kamp {_matchNo:D2} {_homeGoals}-{_awayGoals} " 
+                   + $"{CurrentResult} {isCorrectText}";
+        }
+
+        public bool AddGoal(char teamInput)
+        {
+            var team = char.ToUpper(teamInput);
+            if(!"HB".Contains(team))return false;
+            if (team == 'H') _homeGoals++;
+            else _awayGoals++;
+            return true;
         }
     }
 }
